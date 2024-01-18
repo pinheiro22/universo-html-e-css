c0aa2b1d369ae07d5f8b37e96b8fe05ff29214ad
Gustavo Pinheiro <116049898+pinheiro22@users.noreply.github.com>
2024-01-17 22:37:14 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+const hamburguer = document.querySelector(".hamburguer");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburguer.addEventListener("click", () => {
+    hamburguer.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
+
+
+
+// let navbar = document.querySelector('.navbar');
+
+// document.querySelector('#menu-btn').onclick = () =>{
+//     navbar.classList.toggle('active');
+//     searchForm.classList.remove('active');
+//     cartItem.classList.remove('active');
+// }
+
+document.querySelector('#search-btn').onclick = () =>{
+    searchForm.classList.toggle('active');
+    navbar.classList.remove('active');
+    cartItem.classList.remove('active');
+}
+
+let cartItem = document.querySelector('.cart-items-container');
+
+document.querySelector('#card-btn').onclick = () =>{
+    cartItem.classList.toggle('active');
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
+}
+
+window.onscroll = () =>{
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
+    cartItem.classList.remove('active');
+}
+  
